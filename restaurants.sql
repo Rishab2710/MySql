d79de72c3004793c36b391cb69888945c3d1fe01
Rishab2710 <64508977+Rishab2710@users.noreply.github.com>
2020-12-19 21:21:04 -0500

    Delete restaurants.sql

--- restaurants.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO restaurants(id,name,cuisine,max_price,min_price,rating,location_id)
-VALUES('1','Ivar\'s  Aeres of Clame','Italian',75,35,0.95,3);
-
-INSERT INTO restaurants(cuisine)
-VALUES('mediterranean'),('Middle eastern'),('Greek'),('American'),('Canadian')
-,('French'),('Chineese'),('Indian');
-
-SELECT * FROM restaurants;
