981303653baba321d2690a48f2f9d8859e217553
Rishab2710 <64508977+Rishab2710@users.noreply.github.com>
2020-12-19 21:20:40 -0500

    Delete movies_reviews.sql

--- movies_reviews.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO movies_reviews(id,name,review,rating,movies_id)
-VALUES(1,'The Nice Guy','Amazing Moview will see it again i like depth of characters bla bla bla',
-0.95,1);
-
-INSERT INTO movies_reviews(name,review,rating,movies_id)
-VALUES('Sherlock Holmes','Dont recommend it',
-0.75,1);
-
-SELECT * FROM movies_reviews;
